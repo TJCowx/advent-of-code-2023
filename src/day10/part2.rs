4062e363c8feaba2eea635a18a009e11c17ba86e
Travis <tcowx21@gmail.com>
2023-12-12 14:59:55 -0500

    Comment out stuff that hasn't been worked on

--- src/day10/part2.rs
+++ src/day10/part2.rs
@@ -1,7 +1,7 @@
-use crate::file_reader::file_reader;
+// use crate::file_reader::file_reader;
 
 pub fn solve() {
-    let lines = file_reader::read_as_vec("inputs/day10-test.txt");
+    // let lines = file_reader::read_as_vec("inputs/day10-test.txt");
 
     println!("Day 10 Part 2");
 }
--- src/day12/part2.rs
+++ src/day12/part2.rs
@@ -1,7 +1,7 @@
-use crate::file_reader::file_reader;
+// use crate::file_reader::file_reader;
 
 pub fn solve() {
-    let lines = file_reader::read_as_vec("input/day-12-test.txt");
+    // let lines = file_reader::read_as_vec("input/day-12-test.txt");
 
     println!("Day 12, Part 2");
 }
