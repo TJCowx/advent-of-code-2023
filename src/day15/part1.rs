5cbb325fe7a40c472377a4e511b97a1935d698d7
Travis <tcowx21@gmail.com>
2023-12-16 09:51:08 -0500

    Day 15 part 1

--- src/day15/part1.rs
+++ src/day15/part1.rs
@@ -1,3 +1,38 @@
+use crate::file_reader::file_reader;
+
+static MULTIPLE: u32 = 17;
+static REMAINDER: u32 = 256;
+
+fn get_ascii_value(c: char) -> u32 {
+    c as u32
+}
+
+fn get_sequence_sum(sequence: &str) -> u32 {
+    let mut sum = 0;
+
+    for c in sequence.chars().into_iter() {
+        let val = get_ascii_value(c);
+        sum += val;
+        sum = sum * MULTIPLE;
+        sum = sum % REMAINDER;
+    }
+
+    println!("Sequence: {}, Sum: {}", sequence, sum);
+
+    sum
+}
+
 pub fn solve() {
-    todo!("Haven't worked on this yet");
+    let input = file_reader::read_as_vec("inputs/day15.txt");
+
+    // There is only one line in the input file, so we can unwrap it and split it by commas
+    let sequence: Vec<&str> = input.get(0).unwrap().split(',').collect();
+
+    let mut sum = 0;
+
+    for s in sequence {
+        sum += get_sequence_sum(s);
+    }
+
+    println!("Sum: {}", sum);
 }
